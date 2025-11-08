2882f9f15d00ebf18489d33f2e62ef09b1b6dd45
Pranav Rathi <pranavrathi07@gmail.com>
2025-11-08 10:49:15 +0000

    15:30:54.813 Running build in Washington, D.C., USA (East) – iad1 15:30:

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,8 @@
 
 'use client';
 
-import { useActionState, useFormStatus } from 'react';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
 import { useEffect, useRef } from 'react';
 import { useToast } from '@/hooks/use-toast';
 import { submitContactForm } from './actions';
